d19c4b552e6b3c53333e4f8c5188a73a4fea6bda
Giovana Ellero <giovanaellero.2001@gmail.com>
2023-10-23 22:15:56 -0300

    Update

--- CorridaMuitoMaluca/Assets/Scripts/GameManager.cs
+++ CorridaMuitoMaluca/Assets/Scripts/GameManager.cs
@@ -6,11 +6,16 @@ public class GameManager : MonoBehaviour
 {
     public static List<GameObject> playerCards = new List<GameObject>();
     public static List<GameObject> mapCards = new List<GameObject>();
+    private int AtualPlayer;
+
+    public void Anda(){
+        AtualPlayer++;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        AtualPlayer = 0;
     }
 
     // Update is called once per frame
